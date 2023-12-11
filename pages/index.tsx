d3998458cf8b90a470f6e2be50c738ff47e9ba86
chumchantha <101447078+chumchantha@users.noreply.github.com>
2023-12-11 14:22:13 +0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,43 @@
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import Navbar from "@/components/Navbar";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+
+import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
+const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+
+  return (
+    <>
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
